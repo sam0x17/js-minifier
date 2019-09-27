5748eab4d5a9df0b1b10a3cd94064dc32d82fb4b
Sam Johnson <sam@durosoft.com>
2019-09-27 01:44:44 -0400

    not working

--- src/js-minifier.cr
+++ src/js-minifier.cr
@@ -1,16 +1,14 @@
-require "duktape"
+require "duktape/runtime"
 require "./uglify"
 
 module JsMinifier
+  @@ctx = Duktape::Runtime.new { |ctx| ctx.eval!(UGLIFY_JS) }
+
   def self.minify(source : String)
     source = source.gsub("\"", "\\\"")
     source = source.gsub("\n", "\\n")
-    ctx = Duktape::Sandbox.new
-    src = UGLIFY_JS + "\n" + UGLIFY_DEFAULT_OPTIONS + "\n"
-    src += "var res = minify(\"#{source}\");\n"
-    src += "print(res.error);\n"
-    src += "print(res.code);\n"
-    ctx.eval!(src)
+    @@ctx.eval("var res = minify(\"#{source}\");\n")
+    res = @@ctx.call("res")
   end
 end
 
