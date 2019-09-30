3e581a9587e33729c09c93db6f3213700292a52a
Sam Johnson <sam@durosoft.com>
2019-09-30 02:30:15 -0400

    add spec for multiline minification

--- spec/js-minifier_spec.cr
+++ spec/js-minifier_spec.cr
@@ -5,4 +5,22 @@ describe JsMinifier do
     expect_raises(JsMinifier::SyntaxError) { JsMinifier.minify!("function function") }
     expect_raises(JsMinifier::SyntaxError) { JsMinifier.minify!("var var") }
   end
+
+  it "minifies variable declarations" do
+    JsMinifier.minify!("var blah = true;").should eq "var blah=!0;"
+  end
+
+  it "minifies functions" do
+    JsMinifier.minify!("function foo() { return true; }").should eq "function foo(){return!0}"
+  end
+
+  it "minifies multiline blocks" do
+    src = <<-JS
+      var something = 22;
+      function foo() {
+        return "bar";
+      }
+    JS
+    JsMinifier.minify!(src).should eq "var something=22;function foo(){return\"bar\"}"
+  end
 end
