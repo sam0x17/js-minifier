682d391914c5d6cc2bdd78e012a0dad02f437e8b
Sam Johnson <sam@durosoft.com>
2019-10-03 13:59:15 -0400

    don't load uglify-js js until it is needed

--- src/js-minifier.cr
+++ src/js-minifier.cr
@@ -8,22 +8,24 @@ module JsMinifier
     ctx.eval!(UGLIFY_JS)
     ctx
   end
-  @@ctx : Duktape::Sandbox = initialize_context
+  @@ctx : Duktape::Sandbox? = nil
 
   def self.minify!(source : String, path : String?, options : JSON::Any?)
+    @@ctx ||= initialize_context
+    ctx = @@ctx.not_nil!
     source = source.gsub("\"", "\\\"")
     source = source.gsub("\n", "\\n")
     if options
       options = options.to_json.gsub("'", "\\'")
       puts "var res = minify(\"#{source}\", JSON.stringify('#{options}'));"
-      @@ctx.eval!("var res = minify(\"#{source}\", JSON.stringify('#{options}'));")
+      ctx.eval!("var res = minify(\"#{source}\", JSON.stringify('#{options}'));")
     else
-      @@ctx.eval!("var res = minify(\"#{source}\");")
+      ctx.eval!("var res = minify(\"#{source}\");")
     end
-    @@ctx.eval!("JSON.stringify(res.error)")
-    error = @@ctx.get_string(-1)
-    @@ctx.eval!("res.code")
-    minified = @@ctx.get_string(-1)
+    ctx.eval!("JSON.stringify(res.error)")
+    error = ctx.get_string(-1)
+    ctx.eval!("res.code")
+    minified = ctx.get_string(-1)
     if error
       error = SyntaxError.new(error)
       error.path = path if path
